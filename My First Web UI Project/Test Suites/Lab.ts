d0bdda78474ea678cb37b0636dfe8bf63df8e88a
toan-dp <57466723+toan-dp@users.noreply.github.com>
2020-10-12 23:38:49 +0700

    20201012

---
+++ My First Web UI Project/Test Suites/Lab.ts
@@ -0,0 +1,77 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Lab</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient>;dangphuctoan.k43cnttc.hcmue@gmail.com;testuiweb@gmail.com;</mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>0daf9876-1cbd-41db-82ae-60b6cd38af71</testSuiteGuid>
+   <testCaseLink>
+      <guid>bcea3151-712d-41c7-bc18-1c77a87a9130</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Test Case 1</testCaseId>
+      <testDataLink>
+         <combinationType>ONE</combinationType>
+         <id>5eee0c5e-89ef-49e0-80f6-d4b178867b98</id>
+         <iterationEntity>
+            <iterationType>ALL</iterationType>
+            <value></value>
+         </iterationEntity>
+         <testDataId>Data Files/New Test Data 1</testDataId>
+      </testDataLink>
+      <variableLink>
+         <testDataLinkId>5eee0c5e-89ef-49e0-80f6-d4b178867b98</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>username</value>
+         <variableId>570007de-5372-43c1-9be4-7d392c3e1b84</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId>5eee0c5e-89ef-49e0-80f6-d4b178867b98</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>password</value>
+         <variableId>0a81eb79-3cf3-4044-a06d-e6cce93de5c1</variableId>
+      </variableLink>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>a28446ea-2ad2-4529-842d-6422f381c0e9</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Test Case 2</testCaseId>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>3d0141f9-579c-47d6-9852-7fd582d29ee8</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>2d0e4f27-da27-4b9c-9724-ed929dec8f2a</variableId>
+      </variableLink>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>5e807978-04db-4128-ab7d-98b446aacb1b</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Test Case 3</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>7a42f94f-3f6a-4ad8-8573-dedb610d8041</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Test Case 4</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>8e300dc9-3fee-40c7-89d5-2b48eeb704ac</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Test Case 5</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
